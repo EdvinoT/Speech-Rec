3498360998c45ba46f8caaf04db44a880b748067
EdvinoT <edvinot82@gmail.com>
2026-03-19 12:21:51 +0000

    commit

--- main.py
+++ main.py
@@ -1,21 +1,55 @@
 from gtts import gTTS
 import os
 
-if os == ('nt'):
+print ("Welcome to the Text to Speech Converter!")
+print ("Select language")
+
+languages = [
+    ('en', 'English', 'english'),
+    ('es', 'Spanish', 'spanish'),
+    ('fr', 'French', 'french'),
+    ('de', 'German', 'german'),
+    ('it', 'Italian', 'italian'),
+    ('pt', 'Portuguese', 'portuguese'),
+    ('ja', 'Japanese', 'japanese'),
+    ('ko', 'Korean', 'korean'),
+    ('zh', 'Chinese', 'chinese')
+]
+
+if lang in languages:
+    print(f"Selected language: {i}")
+else:
+    print("Invalid language selection. Defaulting to English.")
+    language = 'en'
+
+def translation():
+    print("Enter the text you want to convert to speech: ")
+    text = input()
+    f = open("text.txt", "w")
+    f.write(text)
+    f.close()
+    
+    f = open("text.txt", "r")
+    
+    input_text = f.read().replace("\n",'')
+   
+    
+    language = 'en'
+
+    voice = gTTS(text=input_text, lang=language, slow=False)
+
+    voice.save("text.mp3")  
+
+    os.system("start text.mp3")
+    
+    
+    
+    return input_text
+
+if os.name == ('nt'):
     os.system('cls')
+
 else:
     os.system('clear')
 
-language = 'en'
-
-f = open("text.txt", "r")
-
-input_text = f.read().replace("\n",'')
-
-language = 'en'
-
-voice = gTTS(text=input_text, lang=language, slow=False)
-
-voice.save("text.mp3")  
-
-os.system("start text.mp3")
+translation()
